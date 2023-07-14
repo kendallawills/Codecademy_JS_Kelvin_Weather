ab9a1dff2f4a6755432b745d3540d660680d8c54
Kendall <kendallawills@gmail.com>
2023-07-14 12:39:02 -0400

    Final Push

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const celsius = kelvin - 273;
 let fahrenheit = celsius * (9 / 5) + 32;
 // Fahrenheit in Round Number
 fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+// Temperature in Newton
+let newton = celsius * (33 / 100);
+// Newton in Round Number
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees Newton.`);
